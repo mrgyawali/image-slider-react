e3ba395f720dba517f9c044736127177f86c8a0b
Aditya Gyawali <aditya@rensoriginal.com>
2019-03-28 16:21:05 +0200

    inital ==>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
 
-class App extends Component {
+import "./App.scss";
+
+import img1 from "./assets/pic1.png";
+import img2 from "./assets/pic2.png";
+import img3 from "./assets/pic3.png";
+
+const images = [
+  {
+    src: img1
+  },
+  { src: img2 },
+  { src: img3 }
+];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0
+    };
+  }
+
+  componentDidMount() {
+    this.startIntervalRef();
+  }
+
+  startIntervalRef = () => {
+    this.intervalRef = setInterval(() => {
+      const { activeIndex } = this.state;
+      this.setState({
+        activeIndex: activeIndex < images.length - 1 ? activeIndex + 1 : 0
+      });
+    }, 5000);
+  };
+
+  changeCurrentImage = i => {
+    this.setState({
+      activeIndex: i
+    });
+    clearInterval(this.intervalRef);
+    this.startIntervalRef();
+  };
+
   render() {
+    const { activeIndex } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="hero-wrapper pt-5">
+        <ul className="slick-dots">
+          {images.map((image, i) => (
+            <li
+              key={image.id}
+              onClick={() => this.changeCurrentImage(i)}
+              className={activeIndex === i ? "active" : ""}
+            />
+          ))}
+        </ul>
+        {images.map((image, index) => (
+          <div
+            className={`hero-background ${
+              activeIndex === index ? "" : "hidden"
+            }`}
+            key={index}
+            style={{
+              backgroundImage: `url(${image.src})`,
+              backgroundSize: "cover"
+            }}
+          />
+        ))}
+
+        <div className="hero-content">
+          <h1>WORLDS FIRST SNEAKER MADE FROM COFFEE</h1>
+        </div>
       </div>
     );
   }
---
+++ src/data.js
@@ -0,0 +1,5 @@
+export const data = [
+  { id: 1, src: "pic1.png", name: "pic1" },
+  { id: 2, src: "pic2.png", name: "pic2" },
+  { id: 3, src: "pic3.png", name: "pic3" }
+];
