7451b7ba9546aac9ba089586669e75562281859e
Aditya Gyawali <aditya@rensoriginal.com>
2019-03-29 15:01:21 +0200

    chagne image variant

--- src/App.js
+++ src/App.js
@@ -7,13 +7,7 @@ import img1 from "./assets/pic1.png";
 import img2 from "./assets/pic2.png";
 import img3 from "./assets/pic3.png";
 
-const images = [
-  {
-    src: img1
-  },
-  { src: img2 },
-  { src: img3 }
-];
+const images = [{ src: img1 }, { src: img2 }, { src: img3 }];
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -49,40 +43,46 @@ class App extends React.Component {
 
     return (
       <div className="hero-wrapper pt-5">
-        <ul className="slick-dots">
-          {images.map((image, i) => (
-            <li
-              key={i}
-              onClick={() => this.changeCurrentImage(i)}
-              className={activeIndex === i ? "active" : ""}
-            />
-          ))}
-        </ul>
-        {images.map((image, index) => (
-          <div
-            className={`hero-background ${
-              activeIndex === index ? "" : "hidden"
-            }`}
-            key={index}
-            style={{
-              backgroundImage: `url(${image.src})`,
-              backgroundSize: "cover"
-            }}
-          />
-        ))}
-
         <Experiment ref="experiment" name="My Example">
           <Variant name="A">
-            <div className="hero-content">
-              <h1>WORLDS FIRST SNEAKER MADE FROM COFFEE</h1>
-            </div>
+            <ul className="slick-dots">
+              {images.map((image, i) => (
+                <li
+                  key={i}
+                  onClick={() => this.changeCurrentImage(i)}
+                  className={activeIndex === i ? "active" : ""}
+                />
+              ))}
+            </ul>
+            {images.map((image, index) => (
+              <div
+                className={`hero-background ${
+                  activeIndex === index ? "" : "hidden"
+                }`}
+                key={index}
+                style={{
+                  backgroundImage: `url(${image.src})`,
+                  backgroundSize: "cover"
+                }}
+              />
+            ))}
           </Variant>
+
           <Variant name="B">
-            <div className="hero-content">
-              <h1>Hello Wrld</h1>
-            </div>
+            <div
+              className={`hero-background`}
+              key={index}
+              style={{
+                backgroundImage: `url(${images[0]})`,
+                backgroundSize: "cover"
+              }}
+            />
           </Variant>
         </Experiment>
+
+        <div className="hero-content">
+          <h1>World First Sneaker Made From Coffee</h1>
+        </div>
       </div>
     );
   }
